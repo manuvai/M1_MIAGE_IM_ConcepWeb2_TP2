5dc15c345831c7a294cee899155cf57f794d71b9
manuvai <rm90266@gmail.com>
2023-01-24 16:48:03 +0100

    ref: Utilisation du même niveau de tabulation

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from connection_db import create_connection, execute_query, execute_read_query
 from flask import Flask
 from flask import render_template
 
-app =  Flask(__name__)
- 
+app = Flask(__name__)
+
+
 @app.route("/index")
 @app.route("/")
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route("/congres")
 def congres_list():
 
     db_path = './db/bd_congres.db'
     connection = create_connection(db_path)
-  
+
     query = """
     SELECT *
     FROM congres
@@ -24,28 +26,30 @@ def congres_list():
 
     column_names = []
     if (len(list_congres) > 0):
-      column_names = list_congres[0].keys()
+        column_names = list_congres[0].keys()
 
     return render_template('congres/list.html', column_names=column_names, list_elements=list_congres)
+
 
 @app.route("/participants")
 def participants_list():
 
-  db_path = './db/bd_congres.db'
-  connection = create_connection(db_path)
+    db_path = './db/bd_congres.db'
+    connection = create_connection(db_path)
 
-  query = """
-  SELECT *
-  FROM participants
-  """
+    query = """
+    SELECT *
+    FROM participants
+    """
 
-  list_participants = execute_read_query(connection, query)
+    list_participants = execute_read_query(connection, query)
 
-  column_names = []
-  if (len(list_participants) > 0):
-    column_names = list_participants[0].keys()
+    column_names = []
+    if (len(list_participants) > 0):
+        column_names = list_participants[0].keys()
 
-  return render_template('participants/list.html', column_names=column_names, list_elements=list_participants)
+    return render_template('participants/list.html', column_names=column_names, list_elements=list_participants)
+
 
 if (__name__ == '__main__'):
 
@@ -54,4 +58,3 @@ if (__name__ == '__main__'):
     debug = True
 
     app.run(host, port, debug)
-  
