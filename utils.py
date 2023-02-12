dd477b4f6e6beaa1a20c187c81533780302d8340
manuvai <rm90266@gmail.com>
2023-02-12 20:51:15 +0100

    docs: Ajout de documentation

--- utils.py
+++ utils.py
@@ -11,7 +11,14 @@ from Table.ProposerTable import ProposerTable
 from flask import session
 
 def find_activites(ids: list) -> list:
+    """Récupère des activités à partir d'une liste passée en paramètre
 
+    Args:
+        ids (list): La liste des IDs
+
+    Returns:
+        list: La liste des activités trouvées
+    """
     activitesTable = ActivitesTable(Database.get_instance())
 
     result = activitesTable.find_by_ids(ids)
@@ -20,6 +27,14 @@ def find_activites(ids: list) -> list:
     return result
 
 def find_thematiques(ids) -> list:
+    """Récupère des thématiques à partir d'une liste passée en paramètre
+
+    Args:
+        ids (list): La liste des IDs
+
+    Returns:
+        list: La liste des thématiques trouvées
+    """
     thematiquesTable = ThematiquesTable(Database.get_instance())
 
     result = thematiquesTable.find_by_ids(ids)
@@ -28,7 +43,12 @@ def find_thematiques(ids) -> list:
     return result
 
 def add_traiter_line(thematiques_ids: list, congres_id: int):
-    
+    """Ajout des thématiques liées à un congres
+
+    Args:
+        thematiques_ids (list): La liste des IDs des thématiques
+        congres_id (int): L'IDS du congres
+    """
     values = []
     for thematique_id in thematiques_ids:
         values.append([congres_id, thematique_id])
@@ -37,6 +57,11 @@ def add_traiter_line(thematiques_ids: list, congres_id: int):
     traiterTable.insert_lines(values)
 
 def auth() -> bool:
+    """Détermine si l'utilisateur est connecté
+
+    Returns:
+        bool: Bool
+    """
     user_id = session.get('user_id')
     is_auth = not user_id is None
 
@@ -50,6 +75,12 @@ def auth() -> bool:
     return is_auth
 
 def add_proposer_line(activites_ids: list, congres_id: int):
+    """Ajout des activités liées à un congres
+
+    Args:
+        thematiques_ids (list): La liste des IDs des activités
+        congres_id (int): L'IDS du congres
+    """
    
     values = []
     for activite_id in activites_ids:
